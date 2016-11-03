f836d721ce76420422450f568d01dff2a2254ded
LyzhinIvan <lyzhin-ivan@yandex.ru>
2016-11-03 10:53:07 +0500

    Сделал десятую лабу без логирования

---
+++ Labs/Lab10/PriceListExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Lab01;
+
+namespace Lab10
+{
+	static class PriceListExtensions
+	{
+		/// <summary>
+		/// Возвращает шины с ценой больше заданной
+		/// </summary>
+		/// <param name="priceList">Прайс-лист</param>
+		/// <param name="price">Цена</param>
+		/// <returns></returns>
+		public static IEnumerable<Tire> WithPriceGreaterThan(this PriceList<Tire> priceList, double price)
+		{
+			foreach (KeyValuePair<Tire, double> item in priceList)
+			{
+				if (item.Value > price)
+					yield return item.Key;
+			}
+		}
+
+		/// <summary>
+		/// Возвращает шины с диаметром равным заданному
+		/// </summary>
+		/// <param name="priceList">Прайс-лист</param>
+		/// <param name="diameter">Диаметр</param>
+		/// <returns></returns>
+		public static IEnumerable<Tire> WithDiameterEqual(this PriceList<Tire> priceList, int diameter)
+		{
+			foreach (KeyValuePair<Tire, double> item in priceList)
+			{
+				if(item.Key.Diameter==diameter)
+					yield return item.Key;
+			}
+		}
+	}
+}
---
+++ Labs/Lab10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Lab01;
+
+namespace Lab10
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var priceList=  new PriceList<Tire>();
+			priceList.Add(new Tire("Kama1", 160, 70, CarcassType.Radial, 15), 2000);
+			priceList.Add(new Tire("Kama2", 170, 80, CarcassType.Radial, 16), 3000);
+			priceList.Add(new Tire("Kama3", 175, 75, CarcassType.Radial, 15), 2500);
+			priceList.Add(new Tire("Kama4", 165, 65, CarcassType.Radial, 14), 2800);
+			priceList.Add(new Tire("Kama5", 160, 70, CarcassType.Radial, 13), 3500);
+			Console.WriteLine("Diameter = 15");
+			foreach (var tire in priceList.WithDiameterEqual(15))
+			{
+				Console.WriteLine(tire);
+			}
+			Console.WriteLine("Price > 2500");
+			foreach (var tire in priceList.WithPriceGreaterThan(2500))
+			{
+				Console.WriteLine(tire);
+			}
+		}
+	}
+}
